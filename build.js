4ba3483f837cc882776d1fb06f380192bb870649
Milan Darjanin <me@milandarjanin.com>
2015-07-13 10:04:49 +0200

    New plugins and added styles
    - metalsmith-assets added
    - ignored log files
    - add github markdown css

--- build.js
+++ build.js
@@ -1,15 +1,16 @@
 var Metalsmith = require('metalsmith')
 var markdown = require('metalsmith-markdown')
 var templates = require('metalsmith-templates')
+var assets = require('metalsmith-assets')
 var archive = require('metalsmith-archive')
 
 Metalsmith(__dirname)
-  .use(archive())
   .use(markdown())
   .use(templates({
     engine: 'swig',
     directory: 'templates'
   }))
+  .use(assets())
   .build(function(err) {
     if (err) throw err;
   })
